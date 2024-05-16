5890ffb8ae609509d3336bef98b7b03e8b7fa9fd
Areeb-Ali <areebithink@gmail.com>
2024-05-16 18:10:12 +0500

    Arrays Completd

---
+++ app.js
@@ -0,0 +1,25 @@
+//-------------------------Variables-------------------------
+
+let myName = "Areeb"; // Declaring a variable named 'myName' and assigning the value "Areeb"
+let age = 17; // Declaring a variable named 'age' and assigning the value 17
+console.log(myName); // Printing my name value
+console.log(age); // Printing my age value
+
+
+
+//-------------------------Data Types-------------------------
+
+let person = {
+    myName: "Areeb",  // Data type string
+    age: 17,  // Data type number
+    isMale: true  // Data type bolean
+}
+
+
+
+//-------------------------Arrays-------------------------
+
+// "[]" is the sign that its an array
+let fruitsArray = ["Banana", "Apple", "Orange", "Grappes"];
+console.log(fruitsArray[0]); // For consoling index number 0 in this it's an banana
+console.log(fruitsArray[2]); // For consoling index number 0 in this it's an Orange
